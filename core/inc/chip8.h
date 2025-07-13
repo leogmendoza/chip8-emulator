d50015aaf6c044d311eacc66a1fa98cb40a36a22
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-07-13 02:44:12 -0400

    Structure (pun intended) the Chip8 struct!

---
+++ core/inc/chip8.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdint.h>
+
+#define REGISTER_COUNT 16
+#define MEMORY_SIZE_BYTES 4096
+#define STACK_DEPTH 16
+#define DISPLAY_HEIGHT 32
+#define DISPLAY_WIDTH 64
+#define DISPLAY_SIZE (DISPLAY_HEIGHT * DISPLAY_WIDTH)
+#define INPUT_KEY_COUNT 16
+
+typedef struct {
+    // CPU
+    uint8_t V[REGISTER_COUNT];      // General-purpose Registers (V0-VF)
+    uint16_t I;                     // Index Register
+    uint16_t pc;                    // Program Counter
+
+    //  Memory
+    uint8_t memory[MEMORY_SIZE_BYTES];
+
+    // Stack
+    uint16_t stack[STACK_DEPTH];
+    uint8_t sp;                     // Stack Pointer
+
+    // Timers
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+
+    // Display
+    uint8_t display[DISPLAY_SIZE];
+
+    // Input
+    uint8_t keypad[INPUT_KEY_COUNT];
+} Chip8;
