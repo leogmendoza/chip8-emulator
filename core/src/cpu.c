aea2ee5178b7de9f4ed2a934a5cd42d38d0527ae
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-08-23 02:09:12 -0400

    Finish  WOO

--- core/src/cpu.c
+++ core/src/cpu.c
@@ -40,13 +40,13 @@ void cpu_init(Chip8 *chip8) {
 
 void cpu_cycle(Chip8 *chip8) {
     // FETCH next instruction from program counter as a two-byte opcode
-    uint16_t opcode = ( chip8->memory[chip8->PC] << 8u | chip8->memory[chip8->PC + 1] );
+    uint16_t opcode = ( chip8->memory[chip8->PC] << 8 | chip8->memory[chip8->PC + 1] );
 
     // Increment program counter before executing
     chip8->PC += 2;
 
     // DECODE and EXECUTE instruction by first extracting leading nibble
-    (table[ (opcode & 0xF000) >> 12u ])(chip8, opcode);
+    (table[ (opcode & 0xF000) >> 12 ])(chip8, opcode);
 
     // Debug instruction executed
     printf(
@@ -88,14 +88,14 @@ static void op_1nnn(Chip8 *chip8, uint16_t opcode) {
 
 // LD Vx, byte: Set register Vx to kk
 static void op_6xkk(Chip8 *chip8, uint16_t opcode) {
-    chip8->V[( opcode & 0x0F00 >> 8u )] = ( opcode & 0x00FF );
+    chip8->V[( opcode & 0x0F00 >> 8 )] = ( opcode & 0x00FF );
 
     return;
 }
 
 // ADD Vx, byte: Increment register Vx by kk
 static void op_7xkk(Chip8 *chip8, uint16_t opcode) {
-    chip8->V[( opcode & 0x0F00 >> 8u )] += ( opcode & 0x00FF );
+    chip8->V[( opcode & 0x0F00 >> 8 )] += ( opcode & 0x00FF );
 
     return;
 }
@@ -109,18 +109,42 @@ static void op_Annn(Chip8 *chip8, uint16_t opcode) {
 
 // DRW Vx, Vy, nibble: Display n-byte sprite from memory location I at (Vx, Vy), set VF = collision
 static void op_Dxyn(Chip8 *chip8, uint16_t opcode) {
-    uint8_t x_pos = chip8->V[( opcode & 0x0F00 >> 8u )] % DISPLAY_WIDTH;
-    uint8_t y_pos = chip8->V[( opcode & 0x00F0 >> 4u )] % DISPLAY_HEIGHT;
+    uint8_t starting_x_pos = chip8->V[( (opcode & 0x0F00) >> 8 )] % DISPLAY_WIDTH;
+    uint8_t starting_y_pos = chip8->V[( (opcode & 0x00F0) >> 4 )] % DISPLAY_HEIGHT;
 
     chip8->V[0xF] = 0;
 
     for (size_t row = 0; row < ( opcode & 0x000F ); row++) {
+        uint8_t current_y_pos = starting_y_pos + row;
         uint8_t sprite_byte = chip8->memory[chip8->I + row];
 
+        // Stop drawing sprite when bottom edge is reached
+        if (current_y_pos >= DISPLAY_HEIGHT) {
+            return;
+        }
+
         for (size_t col = 0; col < 8; col++) {
-            // NOT DONE!
+            uint8_t current_x_pos = starting_x_pos + col;
+
+            // Stop drawing row when right edge is reached
+            if (current_x_pos >= DISPLAY_WIDTH) {
+                break;
+            }
+
+            uint8_t sprite_pixel = ( (sprite_byte >> (7 - col)) & 0b1 );
+
+            if (sprite_pixel) {
+                uint8_t *display_pixel_ptr = &chip8->display[ (current_y_pos * DISPLAY_WIDTH) + current_x_pos ];
+
+                // Flag a collision
+                if (*display_pixel_ptr) {
+                    chip8->V[0xF] = 0b1;
+                } 
+                
+                // Flip pixel
+                *display_pixel_ptr ^= 0b1;
+            }
         }
     }
-
     return;
 }
