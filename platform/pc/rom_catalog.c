bed1ce02c43cc7244079f347bb036f39d29b3751
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-08-27 21:12:58 -0400

    Finally scaled down font.... manually

--- platform/pc/rom_catalog.c
+++ platform/pc/rom_catalog.c
@@ -3,9 +3,9 @@
 #ifdef PLATFORM_PC
 
 const char *rom_list[] = {
-    "roms/tetris.ch8",
-    "roms/space_invaders.ch8",
-    "roms/pong.ch8"
+    "../roms/tetris.ch8",
+    "../roms/space_invaders.ch8",
+    "../roms/pong.ch8"
 };
 
 const int rom_count = (int)(sizeof(rom_list) / sizeof(rom_list[0]));
